2b78f2a328339084ef45ce812ee218122f3b3190
githubKusH <162689983+githubKusH@users.noreply.github.com>
2024-06-08 15:27:06 +0300

    Add files via upload

---
+++ lesson_18/js/script.js
@@ -0,0 +1,8 @@
+
+
+document.addEventListener('click', function (e) {
+	const targetElement = e.target;
+	if (targetElement.closest('.menu-icon')) {
+		document.documentElement.classList.toggle('menu-open');
+	}
+})
